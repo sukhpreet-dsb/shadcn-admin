0363159ad67389c4510d920683d07d4f879b0ebe
sukhpreet-dsb <sukhpreet@designingsolutions.co.in>
2025-02-12 19:26:28 +0530

    config changes

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,9 @@
 import {z} from "zod"
 
-export const env=z.object({
-    VITE_API_URL:z.string().min(1, "API URL is required"),
-}).parse(import.meta.env)
+// export const env=z.object({
+//     VITE_API_URL:z.string().min(1, "API URL is required"),
+// }).parse(import.meta.env)
+
+export const env ={
+    VITE_API_URL:""
+}
